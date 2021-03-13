62abd42f1ac8c4fda9a7ebdefdacbaa0fce95a81
carvalhobruno20 <carvalho.bruno20@gmail.com>
2021-03-13 00:04:56 -0300

    Melhorando o App

--- CalculadoraIMC/app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ CalculadoraIMC/app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -1,19 +1,51 @@
 package com.example.calculadoraimc
 
+import android.content.Context
+import android.icu.text.DecimalFormat
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn.setOnClickListener {
-            val h = (height.text).toString().toFloat() / 100
-            val w = weight.text.toString().toFloat()
-            val res = w / (h * h)
-            result.text = "%.2f".format(res)
+        btnCalc.setOnClickListener {
+            if(etPeso.text.toString() != "" && etAltura.text.toString() != "" ) {
+                val imc = calcIMC(etPeso.text.toString(), etAltura.text.toString())
+                // Seta decimal em apenas 2 casas
+                val df = DecimalFormat("#.00")
+                val imcResp = "IMC: " + df.format(imc) + "\n" + checkIMC(imc)
+                tvResp.text = imcResp
+            }
+            else{
+                tvResp.text = "Valores nulos."
+            }
+            it.hideKeyboard()
         }
+    }
+    // Cálculo do IMC
+    private fun calcIMC(peso: String, altura: String): Double  = peso.toDouble() / (altura.toDouble() * altura.toDouble())
+    // Retorna string de acordo com o cálculo
+    private fun checkIMC(db: Double): String{
+        return when(db) {
+            in 0.0..17.0 -> "Muito abaixo do peso."
+            in 17.1..18.49 -> "Abaixo do peso."
+            in 18.5..24.99 -> "Peso normal."
+            in 25.0..29.99 ->  "Acima do peso."
+            in 30.0..34.99 -> "Obesidade I."
+            in 35.0..39.99 -> "Obesidade II(severa)."
+            else -> "Obesidade III(mórbida)."
+        }
+    }
+    fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
     }
 }
