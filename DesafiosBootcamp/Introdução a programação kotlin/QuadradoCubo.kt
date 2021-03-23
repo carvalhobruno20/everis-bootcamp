8623e278664be1eb79fc0924f39c9b21925fd1b1
carvalhobruno20 <carvalho.bruno20@gmail.com>
2021-03-22 21:08:59 -0300

    Desafios em Kotlin

---
+++ DesafiosBootcamp/Introdução a programação kotlin/QuadradoCubo.kt
@@ -0,0 +1,22 @@
+// Quadrado e ao Cubo
+
+/*
+Você terá o desafio de escrever um programa que leia um valor inteiro N
+(1 < N < 1000). Este N é a quantidade de linhas de saída que serão apresentadas
+na execução do programa.
+- Entrada
+O arquivo de entrada contém um número inteiro positivo N.
+- Saída
+Imprima a saída conforme o exemplo fornecido.
+*/
+
+fun main(args: Array<String>) {
+    //insira as variaveis corretamente
+    var r = 1
+    for (i in 1..readLine()!!.toInt()) {
+
+        println("${i} ${ i * r  } ${ i * r * r }")
+
+        r += 1
+    }
+}
